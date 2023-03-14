fb28b06245f4fbb3623d30f75842285e231484d9
Martin Apiwat <martin.apiwat@gmail.com>
2023-03-14 10:05:38 +0100

    added questionsSchema and created collection

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,4 +1,5 @@
 import express from "express";
+import mongoose from "mongoose";
 
 const app = express();
 
@@ -6,4 +7,29 @@ app.get("/", (req, res) => {
   res.send("Martin");
 });
 
-app.listen(3000);
+const questionSchema = new mongoose.Schema(
+  {
+    title: {
+      type: String,
+      required: true,
+    },
+    description: {
+      type: String,
+      required: true,
+    },
+  },
+  { timestamps: true }
+);
+
+const Question = mongoose.model("questions", questionSchema);
+
+app.get("/questions", async (req, res) => {
+  const questions = await Question.find();
+  res.send(questions);
+});
+
+app.listen(3000, () => {
+  mongoose.connect(
+    "mongodb+srv://MartinApiwat:Martin123@cluster0.rigswq4.mongodb.net/?retryWrites=true&w=majority"
+  );
+});
